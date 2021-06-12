c776270e0581bf1738120fba0493bcd2f7dbe25f
Norman Robinson <normanrobinson@normans-mbp.attlocal.net>
2021-06-12 11:24:50 -0400

    StarterCode/

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,5 +2,11 @@ function buildMetadata(sample) {
     d3.json("samples.json").then(data) => {
         var metadata = data.metadata;
         var resultsarray = metadata.filter(sampleobject => sampleobject.id == sample);
+        var results = resultsarray[0]
+        var PANEL = d3.select('#sample-maetadata');
+        PANEL.html("");
+        Object.entries(result).forEach(([key, value]) => {
+            PANEL.append("h6").text('${key}: ${value');
+        })
     }
 }
