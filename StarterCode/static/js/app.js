f15c14f1a157faf341fb91c5543d2e74b9a3f037
Norman Robinson <normanrobinson@normans-mbp.attlocal.net>
2021-06-12 11:50:57 -0400

    StarterCode/

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -10,3 +10,33 @@ function buildMetadata(sample) {
         })
     }
 }
+
+function buildCharts(sample) {
+    d3.json("samples.json").then((data) => {
+        var samples= data.samples;
+        var resultsarray= samples.filter(sampleobject => sampleobject.id == sample);
+        var result= resultsarray[0]
+    
+        var ids = result.otu_ids;
+        var labels = result.otu_labels;
+        var values = result.sample_values; 
+
+        var bar_data =[
+            {
+              y:ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
+              x:values.slice(0,10).reverse(),
+              text:labels.slice(0,10).reverse(),
+              type:"bar",
+              orientation:"h"
+      
+            }
+          ];
+      
+          var barLayout = {
+            title: "Top 10 Bacteria Cultures Found",
+            margin: { t: 30, l: 150 }
+          };
+      
+          Plotly.newPlot("bar", bar_data, barLayout);
+        });
+        
