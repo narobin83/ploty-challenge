723f44bb2a7fb0305e48e7503032e3ca11cf81b1
Norman Robinson <normanrobinson@normans-mbp.attlocal.net>
2021-06-25 19:50:38 -0400

    intial commit

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -4,15 +4,13 @@ function buildMetadata(sample) {
      
       var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
       var result = resultArray[0];
-      // Use d3 to select the panel with id of `#sample-metadata`
+      
       var PANEL = d3.select("#sample-metadata");
   
-      // Use `.html("") to clear any existing metadata
+     
       PANEL.html("");
   
-      // Use `Object.entries` to add each key and value pair to the panel
-      // Hint: Inside the loop, you will need to use d3 to append new
-      // tags for each key-value in the metadata.
+   
       Object.entries(result).forEach(([key, value]) => {
         PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
       });
