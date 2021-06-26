6af7f935b64036f165aa8cfa9d4d4bcacec30b1a
Norman Robinson <normanrobinson@normans-mbp.attlocal.net>
2021-06-25 20:34:43 -0400

    intial commit

--- StarterCode/static/js/bonus.js
+++ StarterCode/static/js/bonus.js
@@ -1,5 +1,10 @@
 function buildGauge(wfreq) {
     var level = parseFloat(wfreq) * 20;
 
+    var degrees = 180 - level;
+    var radius = 0.5;
+    var radians = (degrees * Math.PI) / 180;
+    var x = radius * Math.cos(radians);
+    var y = radius * Math.sin(radians);
     
 }
